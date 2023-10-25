3b93244a235c0bde8e0c6f0c55460460e9a525ea
Joanjajas <joanbeltranperis@gmail.com>
2023-10-25 18:09:11 +0200

    fixed visual selection comment

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -115,11 +115,13 @@ map("n", "<leader>lgN", function()
   vim.diagnostic.goto_prev()
 end)
 
--- comments
-map("n", "<leader>/", function()
+map("n", "<leader>cm", function()
   require("Comment.api").toggle.linewise.current()
 end)
 
-map("v", "<leader>/", function()
+-- Toggle selection (linewise)
+vim.keymap.set("v", "<leader>cm", function()
+  local esc = vim.api.nvim_replace_termcodes("<esc>", true, false, true)
+  vim.api.nvim_feedkeys(esc, "nx", false)
   require("Comment.api").toggle.linewise(vim.fn.visualmode())
 end)
